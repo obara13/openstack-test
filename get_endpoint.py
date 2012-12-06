4d18ed13bf9e8d7b590d213d2ab222928c905c03
Yuki Obara <obara13@gmail.com>
2012-12-06 12:49:30 +0900

    modified:   get_endpoint.py

--- get_endpoint.py
+++ get_endpoint.py
@@ -8,10 +8,17 @@ url = "localhost:5000"
 
 token = raw_input("token: ")
 tenant = raw_input("tenent: ")
-params = '{ "auth":{ "token":{"id": "%s"}, "tenantId": "%s" } }' % (token, tenant)
+params = '''{
+  "auth": {
+    "token": {
+      "id": "%s"
+    },
+    "tenantId": "%s"
+  }
+}''' % (token, tenant)
 header = {
   "Content-Type": "application/json",
-  "X-Auth-Token": token,
+  "X-Auth-Token": token
 }
 
 session = HTTPConnection(url)
